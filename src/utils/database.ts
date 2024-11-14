baecaea35a311dc0066dd4aea1d239f3bd1f5cb3
alik3dav <alikwotdav@gmail.com>
2024-11-14 10:43:38 +0200

    fix

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 // const DB_PATH = path.join(process.cwd(), 'src/data/data.json');
 const DB_PATH = '/src/data/data.json';
+
 // Ensure this code is only used in server-side logic
 // const DB_PATH = path.join(process.cwd(), 'src/data/data.json');
 
